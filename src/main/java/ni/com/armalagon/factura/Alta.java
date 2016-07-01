7a8dea5adf3cc6c907a0feb0b4d6a4b40143af3b
Armando Alaniz <arjoalaniz@gmail.com>
2016-06-30 22:09:58 -0600

    Eliminar metodo static void main

--- src/main/java/ni/com/armalagon/factura/Alta.java
+++ src/main/java/ni/com/armalagon/factura/Alta.java
@@ -1,7 +1,5 @@
 package ni.com.armalagon.factura;
 
-import java.util.Date;
-
 /**
  *
  * @author armalagon
@@ -22,25 +20,5 @@ public class Alta extends Novedad {
     @Override
     public void setSemana(Semana semana) {
         throw new UnsupportedOperationException("El calculo de la semana es automatico, operacion no soportada");
-    }
-
-    public static void main(String... args) {
-        /*
-        Novedad descanso = new Descanso();
-        descanso.setNss(16146068);
-        descanso.setFechaMovimiento(new Date());
-        descanso.calcularSemana();
-        descanso.setSemana(new Semana(true, true, false, true, false));
-
-        System.out.println("Alta como una alta...Semana: " + descanso.getSemana());
-        */
-
-        Periodo periodo = Periodo.crear(new Date());
-        System.out.println("Periodo: " + periodo + "...# semanas: " + periodo.getTotalSemana());
-
-        /*System.out.println("Periodo #1: " + Configuracion.getInstance().getPeriodo(201601)
-                + ", # semana: " + Configuracion.getInstance().getPeriodo(201601).getTotalSemana());*/
-        System.out.println("Periodo #2: " + Configuracion.getInstance().getPeriodo(201501)
-                + ", # semana: " + Configuracion.getInstance().getPeriodo(201501).getTotalSemana());
     }
 }
