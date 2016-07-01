bcabb05ee1dc40540d725a014d65eb72a764e745
Armando Alaniz <arjoalaniz@gmail.com>
2016-06-30 22:09:13 -0600

    Agregar logger a Configuracion y revertir cambio para obtener periodos del cache

--- src/main/java/ni/com/armalagon/factura/Configuracion.java
+++ src/main/java/ni/com/armalagon/factura/Configuracion.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,6 +14,7 @@ import java.util.concurrent.ConcurrentMap;
  * @since 1.0
  */
 public class Configuracion {
+    private static Logger logger = Logger.getLogger(Configuracion.class.getName());
     private static final Configuracion INSTANCE = new Configuracion();
 
     private final ConcurrentMap<Integer, Periodo> cachePeriodos;
@@ -20,7 +22,7 @@ public class Configuracion {
     private Configuracion() {
         cachePeriodos = new ConcurrentHashMap<>();
 
-        System.out.println("Llenar el cache con los 12 meses de este anio");
+        logger.info("Llenar el cache con los 12 meses de este anio");
         Calendar cal = GregorianCalendar.getInstance();
         int periodo = -1;
         for (int i = 1; i <= 12; i++) {
@@ -32,8 +34,10 @@ public class Configuracion {
 
     public Periodo getPeriodo(int periodo) {
         if (!cachePeriodos.containsKey(periodo)) {
+            logger.info("El periodo solicitado no existe, agregarlo al cache");
             Periodo objPeriodo = Periodo.crear(periodo);
             cachePeriodos.putIfAbsent(periodo, objPeriodo);
+            return objPeriodo;
         }
         return cachePeriodos.get(periodo);
     }
